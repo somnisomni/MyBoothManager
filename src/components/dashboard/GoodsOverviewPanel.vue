4bc30af4017d1f6f2f20b9f13ebf654632dfc35f
somni <me@somni.one>
2023-08-11 22:20:29 +0900

    Add placeholder content in Goods Overview panel

--- src/components/dashboard/GoodsOverviewPanel.vue
+++ src/components/dashboard/GoodsOverviewPanel.vue
@@ -1,6 +1,31 @@
 <template>
   <DashboardPanel title="Goods Overview">
-
+    <VContainer class="flex-column">
+      <VRow>
+        <VCol>
+          <span>Total stocks: 00</span>
+        </VCol>
+        <VCol>
+          <span>Available stocks: 00</span>
+        </VCol>
+      </VRow>
+      <VRow>
+        <VCol>
+          <span>Stocks sold: 00</span>
+        </VCol>
+        <VCol>
+          <span>Stocks given as gift: 00</span>
+        </VCol>
+      </VRow>
+      <VRow>
+        <VCol>
+          <span>Total worth: 00</span>
+        </VCol>
+        <VCol>
+          <span>Sold worth: 00</span>
+        </VCol>
+      </VRow>
+    </VContainer>
   </DashboardPanel>
 </template>
 
