c0219f124c7b46543baa61bfde0d70f3056ee522
somni <me@somni.one>
2023-08-13 23:43:40 +0900

    Add bare booth info edit dialog

---
+++ src/components/navbar/BoothInfoEditDialog.vue
@@ -0,0 +1,58 @@
+<template>
+  <VDialog v-model="open"
+           persistent
+           width="700"
+           max-width="100%"
+           class="booth-info-edit-dialog">
+    <VCard :loading="updateInProgress">
+      <template v-slot:loader="{ isActive }">
+        <VProgressLinear :active="isActive"
+                         indeterminate
+                         color="primary"
+                         height="4" />
+      </template>
+
+      <VCardTitle>부스 정보 수정</VCardTitle>
+
+      <VDivider />
+
+      <VCardText>
+        HERE
+      </VCardText>
+
+      <VDivider />
+
+      <!-- Dialog action -->
+      <VCardActions>
+        <VSpacer />
+        <VBtn :disabled="updateInProgress" text @click="onDialogCancel">취소</VBtn>
+        <VBtn :disabled="updateInProgress" color="primary" text @click="onDialogConfirm">수정하기</VBtn>
+      </VCardActions>
+    </VCard>
+  </VDialog>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Model } from "vue-facing-decorator";
+
+@Component({})
+export default class BoothInfoEditDialog extends Vue {
+  @Model({ type: Boolean, default: false }) open!: boolean;
+
+  updateInProgress = false;
+
+  onDialogCancel() {
+    this.open = false;
+  }
+
+  onDialogConfirm() {
+    // TODO: Replace real API call. Below is a mock.
+
+    this.updateInProgress = true;
+    setTimeout(() => {
+      this.updateInProgress = false;
+      this.open = false;
+    }, 1000);
+  }
+}
+</script>
--- src/components/navbar/BoothSelectionArea.vue
+++ src/components/navbar/BoothSelectionArea.vue
@@ -1,25 +1,29 @@
 <template>
-  <VBtn title="Change the booth to manage" @click.stop="dialogOpen = !dialogOpen">
+  <VBtn title="Change the booth to manage" @click.stop="boothSelectionDialogOpen = !boothSelectionDialogOpen">
     <VToolbarTitle class="text-right text-decoration-underline">{{ boothName }}</VToolbarTitle>
     <VIcon class="ms-4">mdi-refresh</VIcon>
   </VBtn>
-  <VBtn icon class="ms-2 me-0" title="Edit booth information"><VIcon>mdi-storefront-edit</VIcon></VBtn>
+  <VBtn icon class="ms-2 me-0" title="부스 정보 수정" @click.stop="boothInfoEditDialogOpen = !boothInfoEditDialogOpen"><VIcon>mdi-storefront-edit</VIcon></VBtn>
 
-  <BoothSelectionDialog v-model="dialogOpen" />
+  <BoothSelectionDialog v-model="boothSelectionDialogOpen" />
+  <BoothInfoEditDialog v-model="boothInfoEditDialogOpen" />
 </template>
 
 <script lang="ts">
 import { Vue, Component } from "vue-facing-decorator";
 import BoothSelectionDialog from "@/components/navbar/BoothSelectionDialog.vue";
 import { useAdminStore } from "@/stores/admin";
+import BoothInfoEditDialog from "./BoothInfoEditDialog.vue";
 
 @Component({
   components: {
     BoothSelectionDialog,
+    BoothInfoEditDialog,
   },
 })
 export default class BoothSelectionArea extends Vue {
-  dialogOpen = false;
+  boothSelectionDialogOpen = false;
+  boothInfoEditDialogOpen = false;
 
   get boothName() {
     return useAdminStore().boothList[useAdminStore().currentBoothId].name;
