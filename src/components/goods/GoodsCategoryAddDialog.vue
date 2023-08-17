36fbb972037a51362bda6d21825e89400eb35fed
somni <me@somni.one>
2023-08-18 01:35:11 +0900

    Not-yet-completed form checking in Goods Manage dialog
    + Add bare goods category add dialog

---
+++ src/components/goods/GoodsCategoryAddDialog.vue
@@ -0,0 +1,19 @@
+<template>
+  <CommonDialog v-model="open">
+    <h1>TODO</h1>
+  </CommonDialog>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Model } from "vue-facing-decorator";
+import CommonDialog from "@/components/common/CommonDialog.vue";
+
+@Component({
+  components: {
+    CommonDialog,
+  },
+})
+export default class GoodsCategoryAddDialog extends Vue {
+  @Model({ type: Boolean, default: false }) open!: boolean;
+}
+</script>
--- src/components/goods/GoodsManageDialog.vue
+++ src/components/goods/GoodsManageDialog.vue
@@ -1,40 +1,68 @@
 <template>
   <CommonDialog v-model="open"
+                :persistent="isFormEdited"
                 :progressActive="updateInProgress"
                 :hideCloseButton="true"
                 :dialogTitle="dynRes.title"
+                dialogCancelText="취소"
                 :dialogPrimaryText="dynRes.primaryText"
                 :dialogSecondaryText="dynRes.secondaryText"
+                :onDialogCancel="onDialogCancel"
+                :onDialogSecondary="resetForm"
                 :onDialogPrimary="onDialogConfirm"
-                :onDialogSecondary="resetForm">
+                :disableSecondary="!isFormEdited"
+                :disablePrimary="!isFormEdited || !manageFormValid"
+                :closeOnCancel="false">
     <VForm v-model="manageFormValid">
       <VTextField v-model="manageFormData.name"
                   density="compact"
-                  label="굿즈 이름" />
-      <VSelect v-model="manageFormData.categoryId"
-               :items="allCategoryData"
-               item-title="name"
-               item-value="id"
-               label="카테고리" />
+                  label="굿즈 이름"
+                  :rules="stringValidator(manageFormData.name)" />
+      <VRow class="ma-0 d-flex flex-row">
+        <VSelect v-model="manageFormData.categoryId"
+                 class="flex-grow-1"
+                 :items="allCategoryData"
+                 item-title="name"
+                 item-value="id"
+                 label="카테고리"
+                 :rules="[!manageFormData.categoryId ? '카테고리를 선택해주세요.' : true]" />
+        <VBtn icon variant="flat" class="mt-1 ml-2"
+              @click="goodsCategoryAddDialogOpen = !goodsCategoryAddDialogOpen">
+          <VIcon>mdi-plus</VIcon>
+        </VBtn>
+      </VRow>
       <VTextField v-model="manageFormData.price"
                   density="compact"
                   type="number"
                   min="0"
-                  label="가격 (단가)" />
-      <VTextField v-model="manageFormData.stock!.initial"
-                  density="compact"
-                  type="number"
-                  max="10000"
-                  min="0"
-                  label="초기 재고" />
-      <VTextField v-model="manageFormData.stock!.current"
-                  density="compact"
-                  type="number"
-                  :max="manageFormData.stock!.initial"
-                  min="0"
-                  label="현재 재고"
-                  @focus="!manageFormData.stock!.current ? manageFormData.stock!.current = manageFormData.stock!.initial : undefined" />
+                  :prefix="currentBoothCurrencySymbol"
+                  label="가격 (단가)"
+                  :rules="numberValidator(manageFormData.price)" />
+      <VRow class="ma-0 d-flex flex-row">
+        <VTextField v-model="manageFormData.stock!.current"
+                    density="compact"
+                    type="number"
+                    :max="manageFormData.stock!.initial"
+                    min="0"
+                    suffix="개"
+                    label="현재 재고"
+                    :rules="numberValidator(manageFormData.stock!.current)"
+                    @focus="!manageFormData.stock!.current ? manageFormData.stock!.current = manageFormData.stock!.initial : undefined" />
+        <span class="mx-2 mt-1" style="font-size: 1.5em"> / </span>
+        <VTextField v-model="manageFormData.stock!.initial"
+                    density="compact"
+                    type="number"
+                    max="10000"
+                    min="0"
+                    suffix="개"
+                    label="초기 재고"
+                    :rules="numberValidator(manageFormData.stock!.initial)" />
+      </VRow>
     </VForm>
+
+    <GoodsCategoryAddDialog v-model="goodsCategoryAddDialogOpen" />
+    <FormDataLossWarningDialog v-model="cancelWarningDialogShown"
+                               :closeCallback="() => { open = false; }" />
   </CommonDialog>
 </template>
 
@@ -42,12 +70,16 @@
 import { Vue, Component, Model, Prop, Watch } from "vue-facing-decorator";
 import { useAdminStore } from "@/stores/admin";
 import CommonDialog from "@/components/common/CommonDialog.vue";
+import GoodsCategoryAddDialog from "./GoodsCategoryAddDialog.vue";
 import type { GoodsData } from "@/types/goods";
 import { reactive } from "vue";
+import FormDataLossWarningDialog from "@/components/common/FormDataLossWarningDialog.vue";
 
 @Component({
   components: {
     CommonDialog,
+    GoodsCategoryAddDialog,
+    FormDataLossWarningDialog,
   },
 })
 export default class GoodsManageDialog extends Vue {
@@ -55,6 +87,8 @@ export default class GoodsManageDialog extends Vue {
   @Prop({ type: Boolean, default: false }) editMode!: boolean;
   @Prop({ type: Number, default: null, required: true }) goodsId!: number | string | null;
 
+  goodsCategoryAddDialogOpen: boolean = false;
+  cancelWarningDialogShown: boolean = false;
   manageFormData: Partial<GoodsData | Record<string, any>> = reactive({});
   manageFormValid: boolean = false;
   updateInProgress: boolean = false;
@@ -71,24 +105,53 @@ export default class GoodsManageDialog extends Vue {
     return useAdminStore().boothList[useAdminStore().currentBoothId].currencySymbol;
   }
 
+  get currentGoodsData(): GoodsData | null {
+    if(this.goodsId) {
+      return useAdminStore().goodsList[parseInt(this.goodsId.toString())];
+    } else {
+      return null;
+    }
+  }
+
   get allCategoryData() {
     return Object.values(useAdminStore().goodsCategoryList);
+  }
+
+  get isFormEdited(): boolean {
+    // FIXME: Only 1-level nested object( {a:{b:c}} ) will be flatten.
+    const manageFormDataFlatten = Object.values(this.manageFormData).flatMap((v) => {
+      if(v && typeof v === "object") return Object.values(v);
+      else return v;
+    });
+
+    if(!this.editMode && manageFormDataFlatten.some((v) => v)) {
+      return true;
+    } else if(this.editMode && this.currentGoodsData) {
+      // FIXME: Only valid for single-level object( {a:b} ). Not working for current GoodsData type.
+      for(const key in this.manageFormData) {
+        const k = key as keyof GoodsData;
+
+        if(this.manageFormData[k] !== this.currentGoodsData[k]) {
+          return true;
+        }
+      }
+    }
+
+    return false;
   }
 
   mounted() { this.resetForm(); }
   @Watch("open", { immediate: true }) onDialogOpen(watchValue: boolean) { if(watchValue) this.resetForm(); }
 
   resetForm() {
-    if(this.goodsId) {
-      const goodsData = useAdminStore().goodsList[parseInt(this.goodsId.toString())];
-
+    if(this.currentGoodsData) {
       this.manageFormData = reactive({
-        name: goodsData.name,
-        price: goodsData.price,
-        categoryId: goodsData.categoryId,
+        name: this.currentGoodsData.name,
+        price: this.currentGoodsData.price,
+        categoryId: this.currentGoodsData.categoryId,
         stock: {
-          current: goodsData.stock.current,
-          initial: goodsData.stock.initial,
+          current: this.currentGoodsData.stock.current,
+          initial: this.currentGoodsData.stock.initial,
         },
       });
     } else {
@@ -101,6 +164,31 @@ export default class GoodsManageDialog extends Vue {
           initial: null,
         },
       });
+    }
+  }
+
+  stringValidator(input: string): Array<string | boolean> {
+    const rules = [
+      (!input || input.trim().length <= 0) ? "입력한 내용이 없거나 공백으로만 이루어질 수 없습니다." : true,
+    ];
+
+    return rules;
+  }
+
+  numberValidator(input: number): Array<string | boolean> {
+    const rules = [
+      !input ? "숫자를 입력해야 합니다." : true,
+      input < 0 ? "0 이하의 숫자는 입력할 수 없습니다." : true,
+    ];
+
+    return rules;
+  }
+
+  onDialogCancel() {
+    if(this.isFormEdited) {
+      this.cancelWarningDialogShown = true;
+    } else {
+      this.open = false;
     }
   }
 
