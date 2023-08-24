61f678b0b704dd87d5b982f5db2f92584e46c911
somni <me@somni.one>
2023-08-24 23:30:39 +0900

    Export internal types

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,11 @@
 import currencySymbolInfo from "./currency-symbol";
+import { SequelizeInternalKeys, OmitSequelizeInternals } from "./internals";
 
 export {
   /* currency-symbol.ts */
   currencySymbolInfo,
+
+  /* internals.ts */
+  SequelizeInternalKeys,
+  OmitSequelizeInternals,
 };
