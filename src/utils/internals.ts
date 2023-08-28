99ed8fbcd37ae720b995ae3a82dd85d973980ed4
somni <me@somni.one>
2023-08-28 14:05:49 +0900

    Add sequelize internal keys array

--- src/utils/internals.ts
+++ src/utils/internals.ts
@@ -1,7 +1,12 @@
 /**
- * Internal keys that are automatically added and managed by Sequelize.
+ * Type: Internal keys that are automatically added and managed by Sequelize.
  */
 export type SequelizeInternalKeys = "createdAt" | "updatedAt" | "deletedAt";
+
+/**
+ * Array: Internal keys that are automatically added and managed by Sequelize.
+ */
+export const SEQUELIZE_INTERNAL_KEYS = ["createdAt", "updatedAt", "deletedAt"];
 
 /**
  * Helper type to include Sequelize internal keys to a data model interface.
