debd8bd5a8660792c5cc6c37646bc1a238ab04b7
somni <me@somni.one>
2023-09-20 12:36:34 +0900

    backend: Fix booth controller testing def

--- projects/Backend/src/admin/booth/booth.controller.spec.ts
+++ projects/Backend/src/admin/booth/booth.controller.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { BoothController } from "./booth.controller";
 import { BoothService } from "./booth.service";
+import { GoodsModule } from "../goods/goods.module";
 
 describe("BoothController", () => {
   let controller: BoothController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [GoodsModule],
       controllers: [BoothController],
       providers: [BoothService],
     }).compile();
