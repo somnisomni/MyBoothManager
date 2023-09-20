d29ea5c1cf0209d65d825ae14ac5db15cd297c52
somni <me@somni.one>
2023-09-20 22:39:05 +0900

    common: small stuff

--- packages/Common/src/utils/internals.ts
+++ packages/Common/src/utils/internals.ts
@@ -1,5 +1,11 @@
 import { IDataModelBase } from "../interfaces";
 
+interface ISequelizeInternals {
+  createdAt: Date,
+  updatedAt: Date,
+  deletedAt?: Date,
+}
+
 /**
  * Type: Internal keys that are automatically added and managed by Sequelize.
  */
@@ -9,12 +15,6 @@ export type SequelizeInternalKeys = "createdAt" | "updatedAt" | "deletedAt";
  * Array: Internal keys that are automatically added and managed by Sequelize.
  */
 export const SEQUELIZE_INTERNAL_KEYS = ["createdAt", "updatedAt", "deletedAt"];
-
-interface ISequelizeInternals {
-  createdAt: Date,
-  updatedAt: Date,
-  deletedAt?: Date,
-}
 
 /**
  * Helper type to include Sequelize internal keys to a data model interface.
