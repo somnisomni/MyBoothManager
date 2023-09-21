c16fd8fc26c0754386340472851351b8291a1438
somni <me@somni.one>
2023-09-21 10:29:25 +0900

    admin: Add logout button in error dialog of load data overlay

--- projects/Admin/src/pages/BoothAdminLoadDataOverlay.vue
+++ projects/Admin/src/pages/BoothAdminLoadDataOverlay.vue
@@ -7,9 +7,11 @@
   <CommonDialog v-model="errorDialogShown"
                 width="500"
                 dialogPrimaryText="재시도"
+                dialogSecondaryText="로그아웃"
                 :onDialogPrimary="onErrorDialogPrimary"
+                :onDialogSecondary="onErrorDialogSecondary"
                 dialogTitle="데이터 불러오기 오류"
-                accentColor="red">
+                accentColor="green">
     <div class="text-red font-bold">서버로부터 데이터를 불러오는 중 오류가 발생했습니다.</div>
     <div>나중에 다시 시도하거나, <a :href="developerTwitterUrl">트위터 {{ developerTwitterHandle }}</a>로 문의해주세요.</div>
   </CommonDialog>
@@ -19,6 +21,7 @@
 import { Component, Vue } from "vue-facing-decorator";
 import { DEVELOPER_TWITTER_HANDLE } from "@myboothmanager/common";
 import { useAdminStore } from "@/stores/admin";
+import router from "@/router";
 
 @Component({})
 export default class BoothAdminLoadDataOverlay extends Vue {
@@ -42,6 +45,10 @@ export default class BoothAdminLoadDataOverlay extends Vue {
 
   onErrorDialogPrimary() {
     window.location.reload();
+  }
+
+  onErrorDialogSecondary() {
+    window.location.href = router.resolve({ name: "logout" }).href || "/logout";
   }
 }
 </script>
