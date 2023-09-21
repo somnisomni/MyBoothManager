b1d0870de88f74e227ecc949c9e610331370e141
somni <me@somni.one>
2023-09-21 13:53:50 +0900

    admin: Make some of goods overview values to be calculated

--- projects/Admin/src/components/dashboard/GoodsOverviewPanel.vue
+++ projects/Admin/src/components/dashboard/GoodsOverviewPanel.vue
@@ -3,26 +3,26 @@
     <VContainer class="flex-column">
       <VRow>
         <VCol>
-          <span>총 재고 개수: 00</span>
+          <span>총 재고 개수: {{ totalSumStockCount }}개</span>
         </VCol>
         <VCol>
-          <span>남아있는 재고 개수: 00</span>
-        </VCol>
-      </VRow>
-      <VRow>
-        <VCol>
-          <span>판매 개수: 00</span>
-        </VCol>
-        <VCol>
-          <span>무상 증정 개수: 00</span>
+          <span>남아있는 재고 개수: {{ remainingSumStockCount }}개</span>
         </VCol>
       </VRow>
       <VRow>
         <VCol>
-          <span>총 재고 가치: 00</span>
+          <span>판매 개수: {{ totalSumSellCount }}개</span>
         </VCol>
         <VCol>
-          <span>현재까지 판매된 재고 가치: 00</span>
+          <span>무상 증정 개수: 00개</span>
+        </VCol>
+      </VRow>
+      <VRow>
+        <VCol>
+          <span>총 재고 가치: {{ currencySymbol }}{{ totalSumStockValue }}</span>
+        </VCol>
+        <VCol>
+          <span>현재까지 판매된 재고 가치: {{ currencySymbol }}{{ totalSumSellValue }}</span>
         </VCol>
       </VRow>
     </VContainer>
@@ -30,7 +30,9 @@
 </template>
 
 <script lang="ts">
+import type { IGoods } from "@myboothmanager/common";
 import { Component, Vue } from "vue-facing-decorator";
+import { useAdminStore } from "@/stores/admin";
 import DashboardPanel from "./DashboardPanel.vue";
 
 @Component({
@@ -38,5 +40,45 @@ import DashboardPanel from "./DashboardPanel.vue";
     DashboardPanel,
   },
 })
-export default class GoodsOverviewPanel extends Vue { }
+export default class GoodsOverviewPanel extends Vue {
+  private goodsListForEach(callback: (goods: IGoods) => void) {
+    Object.values(useAdminStore().boothGoodsList).forEach((goods) => {
+      callback(goods);
+    });
+  }
+
+  get currencySymbol(): string {
+    return useAdminStore().boothList[useAdminStore().currentBoothId].currencySymbol;
+  }
+
+  get totalSumStockCount(): string {
+    let count = 0;
+    this.goodsListForEach((goods) => { count += goods.stockInitial; });
+    return count.toLocaleString();
+  }
+
+  get remainingSumStockCount(): string {
+    let count = 0;
+    this.goodsListForEach((goods) => { count += goods.stockRemaining; });
+    return count.toLocaleString();
+  }
+
+  get totalSumSellCount(): string {
+    let count = 0;
+    this.goodsListForEach((goods) => { count += goods.stockInitial - goods.stockRemaining; });
+    return count.toLocaleString();
+  }
+
+  get totalSumStockValue(): string {
+    let value = 0;
+    this.goodsListForEach((goods) => { value += goods.price * goods.stockInitial; });
+    return value.toLocaleString();
+  }
+
+  get totalSumSellValue(): string {
+    let value = 0;
+    this.goodsListForEach((goods) => { value += goods.price * (goods.stockInitial - goods.stockRemaining); });
+    return value.toLocaleString();
+  }
+}
 </script>
