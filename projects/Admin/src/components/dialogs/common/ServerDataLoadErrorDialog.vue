7144b689dbcda54a4e9bfdc6d478e616ce4383af
somni <me@somni.one>
2023-10-19 09:01:12 +0900

    admin: Componentize data load error dialog

---
+++ projects/Admin/src/components/dialogs/common/ServerDataLoadErrorDialog.vue
@@ -0,0 +1,43 @@
+<template>
+  <CommonDialog v-model="open"
+                width="500"
+                persistent
+                disableCancel
+                hideCloseButton
+                dialogPrimaryText="새로 고침"
+                dialogSecondaryText="로그아웃"
+                :onDialogPrimary="onErrorDialogPrimary"
+                :onDialogSecondary="onErrorDialogSecondary"
+                dialogTitle="데이터 불러오기 오류"
+                accentColor="green">
+    <div class="text-red font-bold">서버로부터 데이터를 불러오는 중 오류가 발생했습니다.</div>
+    <div>나중에 다시 시도하거나, <a :href="developerTwitterUrl" target="_blank">트위터 {{ developerTwitterHandle }}</a>로 문의해주세요.</div>
+  </CommonDialog>
+</template>
+
+<script lang="ts">
+import { DEVELOPER_TWITTER_HANDLE } from "@myboothmanager/common";
+import { Component, Model, Vue } from "vue-facing-decorator";
+import router from "@/router";
+
+@Component({})
+export default class ServerDataLoadErrorDialog extends Vue {
+  @Model({ type: Boolean, default: false }) open!: boolean;
+
+  get developerTwitterHandle(): string {
+    return `@${DEVELOPER_TWITTER_HANDLE}`;
+  }
+
+  get developerTwitterUrl(): string {
+    return `https://twitter.com/${DEVELOPER_TWITTER_HANDLE}`;
+  }
+
+  onErrorDialogPrimary() {
+    window.location.reload();
+  }
+
+  onErrorDialogSecondary() {
+    window.location.href = router.resolve({ name: "logout" }).href || "/logout";
+  }
+}
+</script>
--- projects/Admin/src/pages/BoothAdminLoadDataOverlay.vue
+++ projects/Admin/src/pages/BoothAdminLoadDataOverlay.vue
@@ -5,41 +5,22 @@
     <div class="text-h6">데이터를 불러오는 중...</div>
   </VContainer>
 
-  <CommonDialog v-model="errorDialogShown"
-                width="500"
-                persistent
-                disableCancel
-                hideCloseButton
-                dialogPrimaryText="새로 고침"
-                dialogSecondaryText="로그아웃"
-                :onDialogPrimary="onErrorDialogPrimary"
-                :onDialogSecondary="onErrorDialogSecondary"
-                dialogTitle="데이터 불러오기 오류"
-                accentColor="green">
-    <div class="text-red font-bold">서버로부터 데이터를 불러오는 중 오류가 발생했습니다.</div>
-    <div>나중에 다시 시도하거나, <a :href="developerTwitterUrl" target="_blank">트위터 {{ developerTwitterHandle }}</a>로 문의해주세요.</div>
-  </CommonDialog>
+  <ServerDataLoadErrorDialog v-model="errorDialogShown" />
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-facing-decorator";
-import { DEVELOPER_TWITTER_HANDLE } from "@myboothmanager/common";
 import { useAdminStore } from "@/stores/admin";
-import router from "@/router";
+import ServerDataLoadErrorDialog from "@/components/dialogs/common/ServerDataLoadErrorDialog.vue";
 
 @Component({
   emits: ["completed"],
+  components: {
+    ServerDataLoadErrorDialog,
+  },
 })
 export default class BoothAdminLoadDataOverlay extends Vue {
   errorDialogShown = false;
-
-  get developerTwitterHandle(): string {
-    return `@${DEVELOPER_TWITTER_HANDLE}`;
-  }
-
-  get developerTwitterUrl(): string {
-    return `https://twitter.com/${DEVELOPER_TWITTER_HANDLE}`;
-  }
 
   async mounted() {
     if(!useAdminStore().currentAccount) {
@@ -54,14 +35,6 @@ export default class BoothAdminLoadDataOverlay extends Vue {
     } else {
       this.errorDialogShown = true;
     }
-  }
-
-  onErrorDialogPrimary() {
-    window.location.reload();
-  }
-
-  onErrorDialogSecondary() {
-    window.location.href = router.resolve({ name: "logout" }).href || "/logout";
   }
 }
 </script>
