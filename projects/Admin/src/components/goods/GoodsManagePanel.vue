a39fbbb6246a8684a227957720803fd007ac6fc1
somni <me@somni.one>
2023-11-01 20:53:15 +0900

    admin: Add 'import goods from file' button without functionality

--- projects/Admin/src/components/goods/GoodsManagePanel.vue
+++ projects/Admin/src/components/goods/GoodsManagePanel.vue
@@ -7,7 +7,15 @@
       <VBtn class="flex-grow-1"
             variant="outlined"
             size="x-large"
-            @click.stop="goodsAddDialogOpen = !goodsAddDialogOpen">굿즈 추가</VBtn>
+            @click.stop="goodsAddDialogOpen = !goodsAddDialogOpen"><VIcon>mdi-plus</VIcon> 굿즈 추가</VBtn>
+      <VBtn class="ml-4 px-0"
+            variant="outlined"
+            min-width="64px"
+            size="x-large"
+            @click.stop="onLoadGoodsFromFileClick">
+        <VTooltip activator="parent" location="bottom">파일로부터 굿즈 목록 불러오기</VTooltip>
+        <VIcon>mdi-file-upload</VIcon>
+      </VBtn>
     </VRow>
   </DashboardPanel>
 
@@ -31,6 +39,10 @@ export default class GoodsManagePanel extends Vue {
 
   get goodsCount(): string {
     return Object.keys(useAdminStore().boothGoodsList).length.toLocaleString();
+  }
+
+  onLoadGoodsFromFileClick(): void {
+    alert("준비 중인 기능입니다.");
   }
 }
 </script>
