0653a849c304c01cd4402536e0b6587c4c532fb6
somni <me@somni.one>
2023-11-20 15:14:32 +0900

    admin: Show warning text too in goods orders page

--- projects/Admin/src/pages/subpages/BoothAdminGoodsOrdersPage.vue
+++ projects/Admin/src/pages/subpages/BoothAdminGoodsOrdersPage.vue
@@ -7,7 +7,8 @@
             @click="onRefreshClick"><VIcon>mdi-refresh</VIcon> 새로고침</VBtn>
     </VLayout>
 
-    <GoodsOrderListView />
+    <GoodsOrderListView v-if="Object.keys(boothGoodsOrders).length > 0" />
+    <h2 v-else-if="!dataLoading && Object.keys(boothGoodsOrders).length <= 0" class="text-center">등록된 판매 기록이 없습니다.</h2>
   </VContainer>
 </template>
 
@@ -32,6 +33,10 @@ export default class BoothAdminGoodsOrdersPage extends Vue {
 
   get boothGoodsOrders() {
     return useAdminStore().boothGoodsOrderList;
+  }
+
+  get boothGoodsOrdersLength(): number {
+    return Object.keys(this.boothGoodsOrders).length;
   }
 
   async onRefreshClick() {
