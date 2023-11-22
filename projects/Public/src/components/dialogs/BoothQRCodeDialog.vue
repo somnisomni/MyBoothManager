4e5f48824355c3b6fbbd110ca9badb57afbeaa85
somni <me@somni.one>
2023-11-22 13:28:26 +0900

    public: Fix QR code not showing
    Fixes #32

--- projects/Public/src/components/dialogs/BoothQRCodeDialog.vue
+++ projects/Public/src/components/dialogs/BoothQRCodeDialog.vue
@@ -1,40 +1,49 @@
 <template>
   <CommonDialog v-model="open"
-                width="auto"
+                width="400px"
                 dialogTitle="부스 페이지 QR 코드">
-    <p class="text-center text-caption">{{ boothPublicUrl }}</p>
-    <canvas ref="qrcode" class="d-block mx-auto"></canvas>
+    <div v-if="boothPublicUrl">
+      <p class="text-center text-caption">{{ boothPublicUrl }}</p>
+      <canvas ref="qrcode" class="d-block mx-auto h-auto" style="max-width: 100%;"></canvas>
+    </div>
+    <div v-else>
+      <p class="text-error">QR 생성 불가</p>
+    </div>
   </CommonDialog>
 </template>
 
 <script lang="ts">
-import QRCode, { type QRCodeRenderersOptions } from "qrcode";
+import * as QRCode from "qrcode";
 import { Component, Model, Prop, Ref, Vue, Watch } from "vue-facing-decorator";
+import router from "@/plugins/router";
 
 @Component({})
 export default class BoothQRCodeDialog extends Vue {
   @Model({ type: Boolean }) open!: boolean;
   @Prop({ type: Number, required: true }) boothId!: number;
 
-  @Ref("qrcode") qrcodeRef: HTMLCanvasElement | null = null;
+  @Ref("qrcode") qrcodeRef?: HTMLCanvasElement;
 
-  readonly qrcodeOptions: QRCodeRenderersOptions = {
+  readonly qrcodeOptions: QRCode.QRCodeRenderersOptions = {
     errorCorrectionLevel: "Q",
     scale: 8,
+    width: 300,
   };
 
   async mounted() { await this.generateQRCode(); }
 
   @Watch("open") @Watch("boothId")
   async generateQRCode() {
-    if(this.boothId > 0 && this.qrcodeRef) {
-      // FIXME: qrcodeRef is null after mounted, so this not works on opening dialog...
+    await this.$nextTick();
+
+    if(this.boothId > 0 && this.qrcodeRef && this.boothPublicUrl) {
       await QRCode.toCanvas(this.qrcodeRef, this.boothPublicUrl, this.qrcodeOptions);
     }
   }
 
-  get boothPublicUrl(): string {
-    return `${window.location.origin}/booth/${this.boothId}`;
+  get boothPublicUrl(): string | null {
+    const url = `${window.location.origin}${router.resolve({ name: "booth-individual", params: { boothId: this.boothId } }).href}`;
+    return URL.canParse(url) ? url : null;
   }
 }
 </script>
