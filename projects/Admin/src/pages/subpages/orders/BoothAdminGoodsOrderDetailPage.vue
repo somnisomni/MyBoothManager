1528891e7c09167847ba920b791228a6909c55cd
somni <me@somni.one>
2023-11-23 13:43:21 +0900

    admin: Basic data presentation in order detail page

--- projects/Admin/src/pages/subpages/orders/BoothAdminGoodsOrderDetailPage.vue
+++ projects/Admin/src/pages/subpages/orders/BoothAdminGoodsOrderDetailPage.vue
@@ -1,22 +1,36 @@
 <template>
-  <VContainer class="mt-4 pa-0 pa-sm-2 pa-md-6">
+  <VContainer class="pa-0 pa-sm-2 pa-md-6">
     <h2 v-if="!orderId" class="text-error">잘못된 접근입니다.</h2>
-    <VLayout v-else class="pa-2 flex-column">
-      <h2 v-if="!orderData">유효하지 않은 판매 기록입니다.</h2>
-      <div v-else>
-        {{ orderId }}
-        {{ orderData.status }}
-        {{ orderData.totalPrice }}
-        {{ orderData.createdAt }}
-        <ul>
-          <li v-for="order in orderData.order" :key="order.gId">
-            {{ order.name }}
-            {{ order.price }}
-            {{ order.quantity }}
-          </li>
-        </ul>
-      </div>
+    <VLayout v-else class="pa-2 justify-start">
+      <VBtn :to="{ name: 'admin-orders' }"
+            size="large"
+            variant="text"
+            prepend-icon="mdi-arrow-left"
+            :active="false"
+            replace>판매 기록 목록으로</VBtn> <!-- :active is workaround -->
     </VLayout>
+
+    <VDivider class="my-2" />
+
+    <div v-if="orderData">
+      <h4 class="mb-2">LAYOUT IS WORK IN PROGRESS</h4>
+
+      <div>ID: {{ orderId }}</div>
+      <div>상태: {{ orderData.status }}</div>
+      <div>총 판매 금액: {{ currencySymbol }}{{ orderData.totalPrice.toLocaleString() }}</div>
+      <div>생성 일자: {{ new Date(orderData.createdAt!).toLocaleString() }}</div>
+
+      <ul class="mt-4">
+        <div>판매 굿즈 목록</div>
+
+        <li v-for="order in orderData.order" :key="order.gId">
+          <span>{{ order.name }}</span> /
+          <span>{{ currencySymbol }}{{ order.price?.toLocaleString() }}</span> /
+          <span>{{ order.quantity.toLocaleString() }}개</span>
+        </li>
+      </ul>
+    </div>
+    <h2 v-else>유효하지 않은 판매 기록입니다.</h2>
   </VContainer>
 </template>
 
@@ -41,6 +55,11 @@ export default class BoothAdminGoodsOrderDetailPage extends Vue {
 
   get orderData(): IGoodsOrder {
     return useAdminStore().boothGoodsOrderList[this.orderId];
+  }
+
+  get currencySymbol(): string {
+    // TODO: Use currency symbol from orderData, after backend is updated
+    return useAdminStore().boothList[this.orderData.boothId].currencySymbol;
   }
 }
 </script>
--- projects/Admin/src/pages/subpages/orders/BoothAdminGoodsOrdersListPage.vue
+++ projects/Admin/src/pages/subpages/orders/BoothAdminGoodsOrdersListPage.vue
@@ -1,11 +1,15 @@
 <template>
-  <VContainer class="mt-4 pa-0 pa-sm-2 pa-md-6">
-    <VLayout class="pa-2">
+  <VContainer class="pa-0 pa-sm-2 pa-md-6">
+    <VLayout class="pa-2 justify-end">
       <VBtn size="large"
+            variant="text"
+            prepend-icon="mdi-refresh"
             :disabled="dataLoading"
             :loading="dataLoading"
-            @click="onRefreshClick"><VIcon>mdi-refresh</VIcon> 새로고침</VBtn>
+            @click="onRefreshClick">새로고침</VBtn>
     </VLayout>
+
+    <VDivider class="my-2" />
 
     <GoodsOrderListView v-if="Object.keys(boothGoodsOrders).length > 0" />
     <h2 v-else-if="!dataLoading && Object.keys(boothGoodsOrders).length <= 0" class="text-center">등록된 판매 기록이 없습니다.</h2>
