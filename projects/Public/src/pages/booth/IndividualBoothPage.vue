1872e003c28680d0d23d9307138c41e3d6b3d279
somni <me@somni.one>
2023-11-28 00:28:37 +0900

    public: Add progress ring while loading data in individual booth page

--- projects/Public/src/pages/booth/IndividualBoothPage.vue
+++ projects/Public/src/pages/booth/IndividualBoothPage.vue
@@ -3,24 +3,31 @@
     <SharePanel :boothData="boothData" />
 
     <h6 class="text-h6 my-2 text-grey text-center">Work in progress!</h6>
-    <VContainer v-if="isDataLoaded">
-      <h4 class="text-h4 text-left">부스 정보</h4>
-      <VDivider class="my-2" />
-      <div>이름: {{ boothData?.name }}</div>
-      <div v-if="boothData?.description">설명: {{ boothData?.description }}</div>
-      <div>위치: {{ boothData?.location }}</div>
-      <div>현재 운영 상태: {{ boothData?.status }}</div>
 
-      <VSpacer class="my-8" />
+    <VSlideYReverseTransition leave-absolute>
+      <VContainer v-if="isDataLoaded">
+        <h4 class="text-h4 text-left">부스 정보</h4>
+        <VDivider class="my-2" />
+        <div>이름: {{ boothData?.name }}</div>
+        <div v-if="boothData?.description">설명: {{ boothData?.description }}</div>
+        <div>위치: {{ boothData?.location }}</div>
+        <div>현재 운영 상태: {{ boothData?.status }}</div>
 
-      <h4 class="text-h4 text-left">굿즈 목록</h4>
-      <VDivider class="my-2" />
-      <GoodsListView :currencySymbol="boothData?.currencySymbol"
-                     :goodsList="boothGoodsList"
-                     :goodsCategoryList="boothCategoryList"
-                     :editable="false"
-                     omitEmptyGoodsCategory />
-    </VContainer>
+        <VSpacer class="my-8" />
+
+        <h4 class="text-h4 text-left">굿즈 목록</h4>
+        <VDivider class="my-2" />
+        <GoodsListView :currencySymbol="boothData?.currencySymbol"
+                      :goodsList="boothGoodsList"
+                      :goodsCategoryList="boothCategoryList"
+                      :editable="false"
+                      omitEmptyGoodsCategory />
+      </VContainer>
+      <div v-else class="w-100 text-center">
+        <VProgressCircular indeterminate color="primary" size="x-large" class="my-2" />
+        <p class="mt-2 text-grey-darken-2">데이터를 불러오는 중입니다...</p>
+      </div>
+    </VSlideYReverseTransition>
   </VMain>
 </template>
 
