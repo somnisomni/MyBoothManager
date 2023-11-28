148e6284269c22424af9a04b1440b22c61c4f033
somni <me@somni.one>
2023-11-28 13:28:30 +0900

    public: Handle data fetch errors in individual booth page

--- projects/Public/src/pages/booth/IndividualBoothPage.vue
+++ projects/Public/src/pages/booth/IndividualBoothPage.vue
@@ -1,11 +1,27 @@
 <template>
   <VMain>
-    <SharePanel :boothData="boothData" />
+    <SharePanel v-if="!dataFetchError" :boothData="boothData" />
 
-    <h6 class="text-h6 my-2 text-grey text-center">Work in progress!</h6>
+    <h6 class="position-fixed text-h6 ma-2 text-grey text-center" style="right: 0; bottom: 0; opacity: 0.5">Work in progress!</h6>
 
     <VSlideYReverseTransition leave-absolute>
-      <VContainer v-if="isDataLoaded">
+      <div v-if="isDataFetched && dataFetchError" class="position-fixed d-flex align-center justify-center w-100 h-100 pa-2 text-center">
+        <h4 class="text-h4 text-center text-error">
+          <VIcon class="mr-2">mdi-alert</VIcon>
+
+          <span v-if="dataFetchError === ErrorCodes.ENTITY_NOT_FOUND">존재하지 않는 부스입니다.</span>
+          <span v-else>데이터를 불러오는 중 오류 발생 ({{ dataFetchError }})</span>
+        </h4>
+      </div>
+
+      <div v-else-if="!isDataFetched" class="position-fixed d-flex flex-column align-center justify-center w-100 h-100 pa-2 text-center">
+        <VProgressCircular indeterminate color="primary" size="x-large" class="my-2" />
+        <p class="mt-2 text-grey-darken-2">데이터를 불러오는 중...</p>
+      </div>
+    </VSlideYReverseTransition>
+
+    <VSlideYReverseTransition leave-absolute>
+      <VContainer v-if="isDataFetched && !dataFetchError">
         <h4 class="text-h4 text-left">부스 정보</h4>
         <VDivider class="my-2" />
         <div>이름: {{ boothData?.name }}</div>
@@ -23,10 +39,6 @@
                       :editable="false"
                       omitEmptyGoodsCategory />
       </VContainer>
-      <div v-else class="w-100 text-center">
-        <VProgressCircular indeterminate color="primary" size="x-large" class="my-2" />
-        <p class="mt-2 text-grey-darken-2">데이터를 불러오는 중입니다...</p>
-      </div>
     </VSlideYReverseTransition>
   </VMain>
 </template>
@@ -34,7 +46,7 @@
 <script lang="ts">
 import { Component, Vue } from "vue-facing-decorator";
 import { useRoute } from "vue-router";
-import { type IBooth, type IGoods, type IGoodsCategory } from "@myboothmanager/common";
+import { ErrorCodes, type IBooth, type IGoods, type IGoodsCategory } from "@myboothmanager/common";
 import SharePanel from "@/components/booth/SharePanel.vue";
 import { usePublicStore } from "@/stores/public";
 
@@ -44,7 +56,10 @@ import { usePublicStore } from "@/stores/public";
   },
 })
 export default class IndividualBoothPage extends Vue {
-  isDataLoaded: boolean = false;
+  readonly ErrorCodes = ErrorCodes;
+
+  isDataFetched: boolean = false;
+  dataFetchError: ErrorCodes | null = null;
 
   boothData: IBooth | null = null;
   boothGoodsList: Array<IGoods> = [];
@@ -56,13 +71,28 @@ export default class IndividualBoothPage extends Vue {
 
   async mounted() {
     await this.fetchData();
-    this.isDataLoaded = true;
+    this.isDataFetched = true;
   }
 
   async fetchData() {
-    this.boothData = await usePublicStore().apiCaller.fetchSingleBooth(this.boothId);
-    this.boothGoodsList = await usePublicStore().apiCaller.fetchAllGoodsOfBooth(this.boothId);
-    this.boothCategoryList = await usePublicStore().apiCaller.fetchAllGoodsCategoryOfBooth(this.boothId);
+    /* Fetch booth data first */
+    const boothDataResponse = await usePublicStore().apiCaller.fetchSingleBooth(this.boothId);
+
+    if("errorCode" in boothDataResponse) {
+      this.dataFetchError = boothDataResponse.errorCode;
+      return;
+    } else {
+      this.boothData = boothDataResponse;
+    }
+
+    /* After booth data is fetched successfully, fetch others */
+    const goodsResponse = await usePublicStore().apiCaller.fetchAllGoodsOfBooth(this.boothId);
+    const categoryResponse = await usePublicStore().apiCaller.fetchAllGoodsCategoryOfBooth(this.boothId);
+
+    if(!("errorCode" in goodsResponse || "errorCode" in categoryResponse)) {
+      this.boothGoodsList = goodsResponse;
+      this.boothCategoryList = categoryResponse;
+    }
   }
 }
 </script>
