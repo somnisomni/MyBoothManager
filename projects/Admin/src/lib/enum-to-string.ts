a557eb57ac9d79cd2d0086388ed503f7e460b276
somni <me@somni.one>
2023-11-29 13:23:07 +0900

    admin: Add some enum to string functions

--- projects/Admin/src/lib/enum-to-string.ts
+++ projects/Admin/src/lib/enum-to-string.ts
@@ -1,4 +1,4 @@
-import { BoothStatus } from "@myboothmanager/common";
+import { BoothStatus, GoodsOrderStatus, GoodsStatus } from "@myboothmanager/common";
 
 export function getBoothStatusString(status: BoothStatus): string {
   switch(status) {
@@ -10,6 +10,30 @@ export function getBoothStatusString(status: BoothStatus): string {
       return "운영 일시 중지";
     case BoothStatus.PREPARE:
       return "운영 준비 중";
+    default:
+      return "알 수 없음";
+  }
+}
+
+export function getGoodsStatusString(status: GoodsStatus): string {
+  switch(status) {
+    case GoodsStatus.ON_SALE:
+      return "판매 중";
+    case GoodsStatus.SOLD_OUT:
+      return "품절";
+    case GoodsStatus.PAUSE:
+      return "판매 일시 중지";
+    default:
+      return "알 수 없음";
+  }
+}
+
+export function getGoodsOrderStatusString(status: GoodsOrderStatus): string {
+  switch(status) {
+    case GoodsOrderStatus.RECORDED:
+      return "정상 기록됨";
+    case GoodsOrderStatus.CANCELED:
+      return "취소됨";
     default:
       return "알 수 없음";
   }
