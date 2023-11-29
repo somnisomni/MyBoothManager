4926b9276b18ef73256ffa3c0c364ef4a0cbaf1b
somni <me@somni.one>
2023-11-29 15:31:08 +0900

    public: Layout WIP on booth info section

--- projects/Public/src/components/booth/BoothInfoSection.vue
+++ projects/Public/src/components/booth/BoothInfoSection.vue
@@ -1,9 +1,15 @@
 <template>
-  <VSheet class="w-100">
-    <div>이름: {{ boothData?.name }}</div>
-    <div v-if="boothData?.description">설명: {{ boothData?.description }}</div>
-    <div>위치: {{ boothData?.location }}</div>
-    <div>현재 운영 상태: {{ boothData?.status }}</div>
+  <VSheet class="booth-info-section">
+    <VParallax class="booth-image" :src="'https://picsum.photos/seed/' + boothData.id + '/800/400'">
+      <div class="booth-image-overlay"></div>
+
+      <VLayout class="booth-info-content w-100 h-100 d-flex flex-column justify-end pa-2">
+        <h3>이름: {{ boothData?.name }}</h3>
+        <div v-if="boothData?.description">설명: {{ boothData?.description }}</div>
+        <div>위치: {{ boothData?.location }}</div>
+        <div>현재 운영 상태: {{ boothData?.status }}</div>
+      </VLayout>
+    </VParallax>
   </VSheet>
 </template>
 
@@ -16,3 +22,39 @@ export default class BoothInfoSection extends Vue {
   @Prop({ type: Object, required: true }) boothData!: IBooth;
 }
 </script>
+
+<style lang="scss" scoped>
+.booth-info-section {
+  --booth-info-section-height: 300px;
+
+  position: relative;
+  width: 100%;
+  height: var(--booth-info-section-height);
+
+  .booth-image {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+  }
+
+  .booth-image-overlay {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    background: linear-gradient(rgba(0, 0, 0, 0.33) 33%, rgba(0, 0, 0, 1));
+  }
+
+  .booth-info-content {
+    color: white;
+    z-index: 1;
+  }
+}
+</style>
