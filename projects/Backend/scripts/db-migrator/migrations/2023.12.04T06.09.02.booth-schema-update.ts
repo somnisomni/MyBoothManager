b251008a95579a2781bdb55e026d6fa56a89b1bb
somni <me@somni.one>
2023-12-04 15:29:08 +0900

    backend: Add DB migration script for c58d7be

---
+++ projects/Backend/scripts/db-migrator/migrations/2023.12.04T06.09.02.booth-schema-update.ts
@@ -0,0 +1,38 @@
+/* eslint-disable arrow-body-style */
+
+/* *** Apply goods order model changes made in c58d7be *** */
+/* *** See https://github.com/somnisomni/MyBoothManager/commit/c58d7be467346b1590603eca4c24a71c9d560365 *** */
+
+import { DataTypes } from "sequelize";
+import Booth from "@/db/models/booth";
+import { Migration } from "../umzug";
+
+export const up: Migration = async ({ context }) => {
+  const members = context.addColumn(Booth.name, "members", {
+    type: DataTypes.JSON,
+    allowNull: false,
+    defaultValue: [],
+  });
+  const expenses = context.addColumn(Booth.name, "expenses", {
+    type: DataTypes.JSON,
+    allowNull: false,
+    defaultValue: [],
+  });
+  const dateOpen = context.addColumn(Booth.name, "dateOpen", {
+    type: DataTypes.DATEONLY,
+  });
+  const dateClose = context.addColumn(Booth.name, "dateClose", {
+    type: DataTypes.DATEONLY,
+  });
+
+  await Promise.all([members, expenses, dateOpen, dateClose]);
+};
+
+export const down: Migration = async ({ context }) => {
+  const members = context.removeColumn(Booth.name, "members");
+  const expenses = context.removeColumn(Booth.name, "expenses");
+  const dateOpen = context.removeColumn(Booth.name, "dateOpen");
+  const dateClose = context.removeColumn(Booth.name, "dateClose");
+
+  await Promise.all([members, expenses, dateOpen, dateClose]);
+};
