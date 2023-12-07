1b7978ba2313789ee75ed5cb3648f87eb4359bf4
somni <me@somni.one>
2023-12-07 10:44:20 +0900

    admin: Display total sale count/price in goods order list page

--- projects/Admin/src/pages/subpages/orders/BoothAdminGoodsOrdersListPage.vue
+++ projects/Admin/src/pages/subpages/orders/BoothAdminGoodsOrdersListPage.vue
@@ -1,8 +1,34 @@
 <template>
   <VContainer class="pa-0 pa-sm-2 pa-md-6">
-    <VLayout class="pa-2 justify-end">
+    <VLayout class="pa-2 d-flex align-end">
+      <div>
+        <div class="d-flex align-center text-body-2 text-disabled"><VIcon class="mr-1">mdi-information-box-outline</VIcon> 취소된 기록을 제외하여 계산된 값입니다.</div>
+
+        <VLayout class="mt-2">
+          <VSheet class="mx-1"
+                 variant="outlined"
+                 @click.prevent>
+            <VLayout class="px-2 d-inline-flex flex-column align-center">
+              <small>기록된 총 재고 수량</small>
+              <span class="text-h5 font-weight-bold">{{ totalSaleCount.toLocaleString() }}개</span>
+            </VLayout>
+          </VSheet>
+          <VDivider class="mx-2" vertical />
+          <VSheet class="mx-1"
+                 variant="outlined"
+                 @click.prevent>
+            <VLayout class="px-2 d-inline-flex flex-column align-center">
+              <small>총 매출액</small>
+              <span class="text-h5 font-weight-bold">{{ currencySymbol }}{{ totalSalePrice.toLocaleString() }}</span>
+            </VLayout>
+          </VSheet>
+        </VLayout>
+      </div>
+
+      <VSpacer />
+
       <VBtn size="large"
-            variant="text"
+            variant="outlined"
             prepend-icon="mdi-refresh"
             :disabled="dataLoading"
             :loading="dataLoading"
@@ -18,6 +44,7 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-facing-decorator";
+import { GoodsOrderStatus } from "@myboothmanager/common";
 import { useAdminStore } from "@/stores/admin";
 import GoodsOrderListView from "@/components/goods/GoodsOrderListView.vue";
 
@@ -30,9 +57,7 @@ export default class BoothAdminGoodsOrdersListPage extends Vue {
   dataLoading: boolean = true;
 
   async mounted() {
-    await useAdminStore().fetchGoodsOrdersOfCurrentBooth();
-
-    this.dataLoading = false;
+    await this.onRefreshClick();
   }
 
   get boothGoodsOrders() {
@@ -41,6 +66,21 @@ export default class BoothAdminGoodsOrdersListPage extends Vue {
 
   get boothGoodsOrdersLength(): number {
     return Object.keys(this.boothGoodsOrders).length;
+  }
+
+  get totalSaleCount(): number {
+    return Object.values(this.boothGoodsOrders).filter((order) => order.status !== GoodsOrderStatus.CANCELED).reduce((acc, cur) =>
+      acc + cur.order.reduce((orderAcc, orderCur) =>
+        orderAcc + new Number(orderCur.quantity).valueOf(), 0), 0);
+  }
+
+  get totalSalePrice(): number {
+    return Object.values(this.boothGoodsOrders).filter((order) => order.status !== GoodsOrderStatus.CANCELED).reduce((acc, cur) =>
+      acc + new Number(cur.totalPrice).valueOf(), 0);
+  }
+
+  get currencySymbol(): string {
+    return useAdminStore().boothList[useAdminStore().currentBoothId].currencySymbol;
   }
 
   async onRefreshClick() {
