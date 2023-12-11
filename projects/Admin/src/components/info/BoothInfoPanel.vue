b43a732279bf668236ddb8e296754294c1f2c166
somni <me@somni.one>
2023-12-12 02:52:33 +0900

    admin: Add booth number in booth info panel

--- projects/Admin/src/components/info/BoothInfoPanel.vue
+++ projects/Admin/src/components/info/BoothInfoPanel.vue
@@ -1,9 +1,14 @@
 <template>
   <DashboardPanel title="부스 등록 정보">
-    <div class="text-center w-100">
-      <div class="text-h4 font-weight-medium">{{ boothData.name }}</div>
-      <div v-if="boothData.location" class="text-overline text-grey-darken-1">@ {{ boothData.location }}</div>
-    </div>
+    <VLayout class="d-flex flex-row text-center w-100">
+      <VLayout class="d-flex flex-column">
+        <div class="text-h4 font-weight-medium">{{ boothData.name }}</div>
+        <div v-if="boothData.location" class="text-overline text-grey-darken-1">@ {{ boothData.location }}</div>
+      </VLayout>
+      <VLayout v-if="boothData.boothNumber" class="d-flex flex-column flex-0-1 mx-2">
+        <div class="text-h3">{{ boothData.boothNumber }}</div>
+      </VLayout>
+    </VLayout>
     <VSheet v-if="boothData.description"
             class="my-2 pa-2 text-center text-subtitle-1 bg-blue-grey-lighten-5"
             rounded>
