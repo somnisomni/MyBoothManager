a1b90435ad75d8da0e67760633b72d56813d014d
somni <me@somni.one>
2024-01-10 21:01:30 +0900

    common: Fix MutualExclusive utility type

--- packages/Common/src/utils/index.ts
+++ packages/Common/src/utils/index.ts
@@ -1,8 +1,9 @@
 export type MutualExclusive<TInterface,
                             TKey1 extends keyof TInterface,
                             TKey2 extends keyof TInterface>
-  = (Pick<TInterface, TKey1> & Omit<TInterface, TKey2>)
-    | (Pick<TInterface, TKey2> & Omit<TInterface, TKey1>);
+  =  (({ [key in TKey1]: TInterface[key] } & { [key in TKey2]?: never })
+    | ({ [key in TKey2]: TInterface[key] } & { [key in TKey1]?: never }))
+    & Omit<TInterface, TKey1 | TKey2>;
 
 export { default as APICaller, MAX_UPLOAD_FILE_BYTES } from "./api-caller";
 export * from "./currency-symbol";
