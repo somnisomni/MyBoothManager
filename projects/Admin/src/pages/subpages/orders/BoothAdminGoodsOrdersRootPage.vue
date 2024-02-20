bbc8e834f0da7e2676c03207481fa46790f542e1
somni <me@somni.one>
2024-02-20 12:52:59 +0900

    admin: Fix route param value not changes in goods order detail page due to keep-alive

--- projects/Admin/src/pages/subpages/orders/BoothAdminGoodsOrdersRootPage.vue
+++ projects/Admin/src/pages/subpages/orders/BoothAdminGoodsOrdersRootPage.vue
@@ -2,7 +2,7 @@
   <div class="mt-4">
     <RouterView v-slot="{ Component }">
       <VSlideXReverseTransition leave-absolute>
-        <KeepAlive>
+        <KeepAlive :include="keepAliveIncludeNames">
           <component :is="Component" />
         </KeepAlive>
       </VSlideXReverseTransition>
@@ -12,7 +12,12 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-facing-decorator";
+import BoothAdminGoodsOrdersListPage from "./BoothAdminGoodsOrdersListPage.vue";
 
 @Component({})
-export default class BoothAdminGoodsOrdersRootPage extends Vue { }
+export default class BoothAdminGoodsOrdersRootPage extends Vue {
+  get keepAliveIncludeNames() {
+    return [ BoothAdminGoodsOrdersListPage.name ];
+  }
+}
 </script>
