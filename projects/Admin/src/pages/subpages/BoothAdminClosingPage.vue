60857447a3b10a82e0519d161e229955eff67f2c
somni <me@somni.one>
2024-03-13 10:20:14 +0900

    admin: Add goods/combination sell worth map in closing page

--- projects/Admin/src/pages/subpages/BoothAdminClosingPage.vue
+++ projects/Admin/src/pages/subpages/BoothAdminClosingPage.vue
@@ -15,6 +15,16 @@
         <p>have combination #{{ combinationId }}</p>
       </div>
     </div>
+
+    <h2>goods sell worth map</h2>
+    <div v-for="[ goodsId, worth ] in goodsSellWorthMap" :key="goodsId">
+      <p>goods #{{ goodsId }}: {{ worth }}</p>
+    </div>
+
+    <h2>goods combination sell worth map</h2>
+    <div v-for="[ combinationId, worth ] in goodsCombinationSellWorthMap" :key="combinationId">
+      <p>combination #{{ combinationId }}: {{ worth }}</p>
+    </div>
   </VContainer>
 </template>
 
@@ -26,6 +36,48 @@ import router from "@/plugins/router";
 
 @Component({})
 export default class BoothAdminClosingPage extends Vue {
+  get goodsSellWorthMap(): Map<number, number> {
+    const map = new Map<number, number>();
+
+    for(const item of Object.values(useAdminStore().currentBooth.goodsOrders!)) {
+      for(const goods of Object.values(item.order)) {
+        if(goods.gId) {
+          const originalPrice = useAdminStore().currentBooth.goods![goods.gId]?.price ?? 0;
+          const calculatedPrice = (goods.price ?? originalPrice) * goods.quantity;
+
+          if(map.has(goods.gId)) {
+            map.set(goods.gId, map.get(goods.gId)! + calculatedPrice);
+          } else {
+            map.set(goods.gId, calculatedPrice);
+          }
+        }
+      }
+    }
+
+    return map;
+  }
+
+  get goodsCombinationSellWorthMap(): Map<number, number> {
+    const map = new Map<number, number>();
+
+    for(const item of Object.values(useAdminStore().currentBooth.goodsOrders!)) {
+      for(const combination of Object.values(item.order)) {
+        if(combination.cId) {
+          const originalPrice = useAdminStore().currentBooth.goodsCombinations![combination.cId]?.price ?? 0;
+          const calculatedPrice = (combination.price ?? originalPrice) * combination.quantity;
+
+          if(map.has(combination.cId)) {
+            map.set(combination.cId, map.get(combination.cId)! + calculatedPrice);
+          } else {
+            map.set(combination.cId, calculatedPrice);
+          }
+        }
+      }
+    }
+
+    return map;
+  }
+
   get goodsMemberMap(): Map<number, Array<number>> {
     const map = new Map<number, Array<number>>();
 
