63e552c29888f8191b3651ec4704850b911654df
somni <me@somni.one>
2024-04-29 16:57:47 +0900

    backend: Hide booth ID in booth member response DTO

--- projects/Backend/src/modules/public/booth-member/dto/booth-member.dto.ts
+++ projects/Backend/src/modules/public/booth-member/dto/booth-member.dto.ts
@@ -4,13 +4,14 @@ import { Exclude, Expose } from "class-transformer";
 @Exclude()
 export class BoothMemberResponseDto implements IBoothMemberResponse {
   @Expose() declare id: number;
-  @Expose() declare boothId: number;
   @Expose() declare name: string;
   @Expose() declare descriptionShort?: string;
   @Expose() declare role?: string;
   @Expose() declare primaryColor?: string;
   @Expose() declare url?: string;
   @Expose() declare memberImageUrl?: string;
+
+  @Exclude() declare boothId: number;
 
   constructor(partial: Partial<IBoothMemberModel>) {
     Object.assign(this, partial);
