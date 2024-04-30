2fce8877a3517cfe7adc46133fe11333e3d23dda
somni <me@somni.one>
2024-04-30 20:31:11 +0900

    backend: Fix testing

--- projects/Backend/src/modules/admin/booth/booth.controller.spec.ts
+++ projects/Backend/src/modules/admin/booth/booth.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { PublicBoothModule } from "@/modules/public/booth/booth.module";
+import { PublicBoothMemberModule } from "@/modules/public/booth-member/booth-member.module";
 import { GoodsOrderModule } from "../goods-order/goods-order.module";
 import { UtilModule } from "../util/util.module";
 import { AdminAuthGuard } from "../auth/auth.guard";
@@ -11,7 +12,7 @@ describe("BoothController", () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [PublicBoothModule, GoodsOrderModule, UtilModule],
+      imports: [PublicBoothModule, PublicBoothMemberModule, GoodsOrderModule, UtilModule],
       controllers: [BoothController],
       providers: [BoothService],
     }).overrideGuard(AdminAuthGuard).useValue({ canActivate: () => true }).compile();
