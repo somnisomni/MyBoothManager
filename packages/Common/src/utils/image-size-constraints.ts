9c86acf85b48909a1938e00e4db97b861a87be80
somni <me@somni.one>
2024-04-30 20:27:09 +0900

    common: Fix goods image resize fitting method

--- packages/Common/src/utils/image-size-constraints.ts
+++ packages/Common/src/utils/image-size-constraints.ts
@@ -5,9 +5,9 @@ export enum ImageSizeConstraintKey {
   GOODS,
 }
 
-export const IMAGE_SIZE_CONSTRAINTS: ReadonlyMap<ImageSizeConstraintKey, { width: number, height: number, fit: "cover" | "outside" }> = new Map([
+export const IMAGE_SIZE_CONSTRAINTS: ReadonlyMap<ImageSizeConstraintKey, { width: number, height: number, fit: "cover" | "inside" | "outside" }> = new Map([
   [ImageSizeConstraintKey.BOOTH_BANNER,        { width: 1, height: 600, fit: "outside" }],
   [ImageSizeConstraintKey.BOOTH_INFO,          { width: 1500, height: 1, fit: "outside" }],
   [ImageSizeConstraintKey.BOOTH_MEMBER_AVATAR, { width: 300, height: 300, fit: "cover" }],
-  [ImageSizeConstraintKey.GOODS,               { width: 1500, height: 500, fit: "cover" }],
+  [ImageSizeConstraintKey.GOODS,               { width: 1500, height: 500, fit: "inside" }],
 ]);
