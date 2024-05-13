08c18b1221b12d2cfdf8fbf90839d2375cd0887b
somni <me@somni.one>
2024-05-13 16:09:58 +0900

    public: Fix out of stock indicator not showing in public goods item

--- projects/Public/src/components/goods/GoodsItemPublic.vue
+++ projects/Public/src/components/goods/GoodsItemPublic.vue
@@ -3,7 +3,7 @@
              class="public">
     <template #extra-top-indicator>
       <!-- Out of stock indicator -->
-      <div v-if="isOutOfStock" class="top-indicator out-of-stock bg-error">
+      <div v-if="isMounted && !shouldHideStock && isOutOfStock" class="top-indicator out-of-stock bg-error">
         <span>품 절</span>
       </div>
     </template>
@@ -11,20 +11,27 @@
 </template>
 
 <script lang="ts">
-import type { GoodsItem, GoodsItemProps } from "@myboothmanager/common-ui";
+import type { GoodsItem } from "@myboothmanager/common-ui";
+import { GoodsStockVisibility } from "@myboothmanager/common";
 import { Component, Vue, Ref } from "vue-facing-decorator";
 
 @Component({})
 export default class GoodsItemPublic extends Vue {
+  isMounted: boolean = false;
+
   @Ref("base")
   readonly baseComponent!: GoodsItem;
 
-  get baseProps(): GoodsItemProps {
-    return this.baseComponent?.$props as GoodsItemProps;
+  get isOutOfStock(): boolean {
+    return this.baseComponent.normalizedData.stockRemaining <= 0;
   }
 
-  get isOutOfStock(): boolean {
-    return this.baseProps?.normalizedData.stockRemaining <= 0;
+  get shouldHideStock(): boolean {
+    return this.baseComponent.normalizedData.stockVisibility === GoodsStockVisibility.HIDE_ALL;
+  }
+
+  mounted() {
+    this.isMounted = true;
   }
 }
 </script>
