a4735f81928e352da60b3b789e1aae2adcdb65ad
somni <me@somni.one>
2024-05-19 19:01:01 +0900

    admin: superadmin chore

--- projects/Admin/src/pages/superadmin/SuperAdminPage.vue
+++ projects/Admin/src/pages/superadmin/SuperAdminPage.vue
@@ -1,6 +1,7 @@
 <template>
   <VContainer>
-    <VBtn :to="{ name: 'logout' }">로그아웃</VBtn>
+    <p class="my-2 text-warning">※ 서비스 관리 계정으로 로그인했습니다. 보안을 위해 페이지를 새로고침하거나 닫으면 즉시 로그인 세션이 종료됩니다.</p>
+    <VBtn :to="{ name: 'logout' }" prepend-icon="mdi-logout">로그아웃</VBtn>
 
     <VLayout class="d-flex flex-column my-8 pa-2">
       <h1>부스 관리자 계정 생성</h1>
