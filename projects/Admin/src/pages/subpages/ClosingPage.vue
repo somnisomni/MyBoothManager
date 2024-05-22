b72d6204e26c52b5c22dce2f83db7c2b99aa80ab
somni <me@somni.one>
2024-05-23 02:05:05 +0900

    admin: WIP closing page

--- projects/Admin/src/pages/subpages/ClosingPage.vue
+++ projects/Admin/src/pages/subpages/ClosingPage.vue
@@ -1,5 +1,61 @@
 <template>
   <VContainer class="mt-4 pa-2 pa-md-6">
+    <div class="mb-4">
+      <ul style="list-style: none">
+        <li v-for="[ combinationId, data ] in goodsCombinationRevenueMap"
+            :key="combinationId"
+            class="d-flex justify-space-between">
+          <span><small class="d-inline-block text-right mr-1" style="width: 4em; white-space: nowrap; text-wrap: nowrap;">{{ data.quantity }} ×</small> <VIcon icon="mdi-set-all" /> {{ data.name }}</span>
+          <span>{{ currencySymbol }}{{ data.total.toLocaleString() }} </span>
+        </li>
+
+        <li v-for="[ goodsId, data ] in goodsRevenueMap"
+            :key="goodsId"
+            class="d-flex justify-space-between">
+          <span><small class="d-inline-block text-right mr-1" style="width: 4em; white-space: nowrap; text-wrap: nowrap;">{{ data.quantity }} ×</small> {{ data.name }}</span>
+          <span>{{ currencySymbol }}{{ data.total.toLocaleString() }}</span>
+        </li>
+
+        <li class="d-flex justify-space-between font-weight-bold">
+          <span><small class="d-inline-block text-right mr-1" style="width: 4em;"></small> 총 판매 수익</span>
+          <span>{{ currencySymbol }}{{ totalRevenue.toLocaleString() }}</span>
+        </li>
+      </ul>
+    </div>
+
+    <div v-if="Object.values(currentBooth.boothMembers ?? { }).length > 0"
+         class="mb-4">
+      <VTabs v-model="memberRevenueDistributionStrategy"
+             fixed-tabs>
+        <VTab value="own">소유 굿즈별 분배</VTab>
+        <VTab value="equal">동률 분배 (1/N)</VTab>
+      </VTabs>
+
+      <VTabsWindow v-model="memberRevenueDistributionStrategy">
+        <VTabsWindowItem value="own">
+          <ul style="list-style: none">
+            <li v-for="[ memberId, revenue ] in memberRevenueMap"
+                :key="memberId"
+                class="d-flex justify-space-between">
+              <span>{{ currentBooth.boothMembers![memberId].name }}</span>
+              <span>{{ currencySymbol }}{{ revenue.toLocaleString() }}</span>
+            </li>
+          </ul>
+        </VTabsWindowItem>
+
+        <VTabsWindowItem value="equal">
+          <ul style="list-style: none">
+            <li v-for="member in currentBooth.boothMembers"
+                :key="member.id"
+                class="d-flex justify-space-between">
+              <span>{{ member.name }}</span>
+              <span>{{ currencySymbol }}{{ (totalRevenue / Object.values(currentBooth.boothMembers!).length).toLocaleString() }}</span>
+            </li>
+          </ul>
+        </VTabsWindowItem>
+      </VTabsWindow>
+    </div>
+
     <h2>goods</h2>
     <div v-for="[ memberId, goodsIds ] in goodsMemberMap" :key="memberId">
       <h3>member ID #{{ memberId }}</h3>
@@ -17,43 +73,51 @@
     </div>
 
     <h2>goods sell worth map</h2>
-    <div v-for="[ goodsId, data ] in goodsSellWorthMap" :key="goodsId">
+    <div v-for="[ goodsId, data ] in goodsRevenueMap" :key="goodsId">
       <p>goods #{{ goodsId }}: {{ data.quantity.toLocaleString() }}개 / {{ currencySymbol }}{{ data.total.toLocaleString() }}</p>
     </div>
 
     <h2>goods combination sell worth map</h2>
-    <div v-for="[ combinationId, data ] in goodsCombinationSellWorthMap" :key="combinationId">
+    <div v-for="[ combinationId, data ] in goodsCombinationRevenueMap" :key="combinationId">
       <p>combination #{{ combinationId }}: {{ data.quantity.toLocaleString() }} 개 / {{ currencySymbol }}{{ data.total.toLocaleString() }}</p>
     </div>
   </VContainer>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-facing-decorator";
+import { Component, Setup, Vue } from "vue-facing-decorator";
 import { useAdminStore } from "@/plugins/stores/admin";
+
+type GoodsWorthMapItem = { name: string, quantity: number, total: number };
 
 @Component({})
 export default class ClosingPage extends Vue {
-  get currencySymbol(): string {
-    return useAdminStore().currentBooth.booth?.currencySymbol ?? "₩";
-  }
+  @Setup(() => useAdminStore().currentBooth)
+  declare readonly currentBooth: Readonly<ReturnType<typeof useAdminStore>["currentBooth"]>;
 
-  get goodsSellWorthMap(): Map<number, { quantity: number, total: number }> {
-    const map = new Map<number, { quantity: number, total: number }>();
+  @Setup(() => useAdminStore().currentBooth.booth?.currencySymbol)
+  declare readonly currencySymbol: string;
 
-    for(const item of Object.values(useAdminStore().currentBooth.goodsOrders!)) {
+  memberRevenueDistributionStrategy: "equal" | "own" = "own";
+
+  get goodsRevenueMap(): Map<number, GoodsWorthMapItem> {
+    const map = new Map<number, GoodsWorthMapItem>();
+
+    for(const item of Object.values(this.currentBooth.goodsOrders!)) {
       for(const goods of Object.values(item.order)) {
         if(goods.gId) {
-          const originalPrice = useAdminStore().currentBooth.goods![goods.gId]?.price ?? 0;
+          const originalPrice = this.currentBooth.goods![goods.gId]?.price ?? 0;
           const calculatedPrice = (goods.price ?? originalPrice) * goods.quantity;
 
           if(map.has(goods.gId)) {
             map.set(goods.gId, {
+              name: this.currentBooth.goods![goods.gId]!.name,
               quantity: map.get(goods.gId)!.quantity + goods.quantity,
               total: map.get(goods.gId)!.total + calculatedPrice,
             });
           } else {
             map.set(goods.gId, {
+              name: this.currentBooth.goods![goods.gId]!.name,
               quantity: goods.quantity,
               total: calculatedPrice,
             });
@@ -62,25 +126,27 @@ export default class ClosingPage extends Vue {
       }
     }
 
-    return map;
+    return new Map(Array.from(map.entries()).sort((a, b) => a[1].name.localeCompare(b[1].name)));
   }
 
-  get goodsCombinationSellWorthMap(): Map<number, { quantity: number, total: number }> {
-    const map = new Map<number, { quantity: number, total: number }>();
+  get goodsCombinationRevenueMap(): Map<number, GoodsWorthMapItem> {
+    const map = new Map<number, GoodsWorthMapItem>();
 
-    for(const item of Object.values(useAdminStore().currentBooth.goodsOrders!)) {
+    for(const item of Object.values(this.currentBooth.goodsOrders!)) {
       for(const combination of Object.values(item.order)) {
         if(combination.cId) {
-          const originalPrice = useAdminStore().currentBooth.goodsCombinations![combination.cId]?.price ?? 0;
+          const originalPrice = this.currentBooth.goodsCombinations![combination.cId]?.price ?? 0;
           const calculatedPrice = (combination.price ?? originalPrice) * combination.quantity;
 
           if(map.has(combination.cId)) {
             map.set(combination.cId, {
+              name: this.currentBooth.goodsCombinations![combination.cId]!.name,
               quantity: map.get(combination.cId)!.quantity + combination.quantity,
               total: map.get(combination.cId)!.total + calculatedPrice,
             });
           } else {
             map.set(combination.cId, {
+              name: this.currentBooth.goodsCombinations![combination.cId]!.name,
               quantity: combination.quantity,
               total: calculatedPrice,
             });
@@ -89,15 +155,20 @@ export default class ClosingPage extends Vue {
       }
     }
 
-    return map;
+    return new Map(Array.from(map.entries()).sort((a, b) => a[1].name.localeCompare(b[1].name)));
+  }
+
+  get totalRevenue(): number {
+    return Array.from(this.goodsRevenueMap.values()).reduce((acc, cur) => acc + cur.total, 0) +
+           Array.from(this.goodsCombinationRevenueMap.values()).reduce((acc, cur) => acc + cur.total, 0);
   }
 
   get goodsMemberMap(): Map<number, Array<number>> {
     const map = new Map<number, Array<number>>();
 
-    for(const member of Object.values(useAdminStore().currentBooth.boothMembers ?? {})) {
+    for(const member of Object.values(this.currentBooth.boothMembers ?? {})) {
       map.set(member.id,
-              Object.values(useAdminStore().currentBooth.goods ?? {}).filter(
+              Object.values(this.currentBooth.goods ?? {}).filter(
                 (goods) => goods.ownerMemberIds?.includes(member.id),
               ).map((goods) => goods.id));
     }
@@ -108,9 +179,9 @@ export default class ClosingPage extends Vue {
   get goodsCombinationMemberMap(): Map<number, Array<number>> {
     const map = new Map<number, Array<number>>();
 
-    for(const member of Object.values(useAdminStore().currentBooth.boothMembers ?? {})) {
+    for(const member of Object.values(this.currentBooth.boothMembers ?? {})) {
       map.set(member.id,
-              Object.values(useAdminStore().currentBooth.goodsCombinations ?? {}).filter(
+              Object.values(this.currentBooth.goodsCombinations ?? {}).filter(
                 (goods) => goods.ownerMemberIds?.includes(member.id),
               ).map((goods) => goods.id));
     }
@@ -118,8 +189,25 @@ export default class ClosingPage extends Vue {
     return map;
   }
 
+  get memberRevenueMap(): Map<number, number> {
+    const map = new Map<number, number>();
+
+    for(const member of Object.values(this.currentBooth.boothMembers ?? {})) {
+      const goodsTotal = this.goodsMemberMap.get(member.id)!
+        .map((goodsId) => this.goodsRevenueMap.get(goodsId)!.total)
+        .reduce((acc, cur) => acc + cur, 0);
+      const combinationTotal = this.goodsCombinationMemberMap.get(member.id)!
+        .map((combinationId) => this.goodsCombinationRevenueMap.get(combinationId)!.total)
+        .reduce((acc, cur) => acc + cur, 0);
+
+      map.set(member.id, goodsTotal + combinationTotal);
+    }
+
+    return map;
+  }
+
   beforeCreate() {
-    /* if(useAdminStore().currentBooth.booth && useAdminStore().currentBooth.booth!.status !== BoothStatus.CLOSE) {
+    /* if(this.currentBooth.booth && this.currentBooth.booth!.status !== BoothStatus.CLOSE) {
       alert("부스가 운영 종료 상태일 때만 접근할 수 있습니다.");
 
       router.back();
