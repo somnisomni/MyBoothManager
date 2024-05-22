9a8c4cec948c3271d966d0fd1dbf9e7752d86b16
somni <me@somni.one>
2024-05-22 10:39:57 +0900

    backend: Fix goods combination admin DTO not containing all stock info

--- projects/Backend/src/modules/admin/goods-combination/dto/goods-combination.dto.ts
+++ projects/Backend/src/modules/admin/goods-combination/dto/goods-combination.dto.ts
@@ -1,4 +1,19 @@
-import { IGoodsCombinationResponse } from "@myboothmanager/common";
+import { IGoodsCombinationResponse, IGoodsStock } from "@myboothmanager/common";
+import { Exclude, Expose } from "class-transformer";
 import { PublicGoodsCombinationResponseDto } from "@/modules/public/goods-combination/dto/goods-combination.dto";
+import GoodsCombination from "@/db/models/goods-combination";
 
-export class AdminGoodsCombinationResponseDto extends PublicGoodsCombinationResponseDto implements IGoodsCombinationResponse { }
+@Exclude()
+export class AdminGoodsCombinationResponseDto extends PublicGoodsCombinationResponseDto implements IGoodsCombinationResponse {
+  @Expose() declare stock: Required<IGoodsStock>;
+
+  constructor(model: GoodsCombination) {
+    super(model);
+
+    this.stock = {
+      visibility: model.get("stockVisibility"),
+      initial: model.get("stockInitial"),
+      remaining: model.get("stockRemaining"),
+    };
+  }
+}
