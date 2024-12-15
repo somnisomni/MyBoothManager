5c984a58183bf805ce6ae8131a637ca53ffc2e6f
somni <me@somni.one>
2024-12-15 17:47:11 +0900

    root: Ditch `host` network mode while building containers using compose

--- compose-build.sh
+++ compose-build.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
+# On Ubuntu 22.04, podman-compose version must be 1.0.6 due to missing of latest podman version.
 
 export GIT_COMMIT_HASH=$(git rev-parse --short HEAD)
-COMPOSE=docker-compose
+OCI=podman
+COMPOSE=$OCI-compose
 COMPOSE_FILE=compose.local.yaml
+NETWORK_NAME=myboothmanager
 
 if ! command -v $COMPOSE > /dev/null 2>&1; then
-  if command -v podman-compose > /dev/null 2>&1; then
-    COMPOSE=podman-compose
-  else
-    echo "Error: Neither docker-compose nor podman-compose found."
-    exit 1
-  fi
+  echo "Error: '$COMPOSE' command not found."
+  exit 1
 fi
 
 if ! [ -f "$COMPOSE_FILE" ]; then
@@ -22,10 +21,30 @@ if ! [ -f "$COMPOSE_FILE" ]; then
   fi
 fi
 
+if ! $OCI network exists $NETWORK_NAME > /dev/null; then
+  echo "'$NETWORK_NAME' network not found. Creating..."
+  NETWORK_CREATE_OUTPUT=$($OCI network create $NETWORK_NAME)
+
+  if [ $? -ne 0 ]; then
+    echo "Error: Failed to create '$NETWORK_NAME' network."
+    exit 1
+  fi
+
+  if [ -f "$NETWORK_CREATE_OUTPUT" ]; then
+    echo "Fixing CNI version to 0.4.0..."
+    sed -i 's/\(\"cniVersion\"\:\) \"\([[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+\)\"/\1 \"0.4.0\"/' $NETWORK_CREATE_OUTPUT
+  else
+    echo "Warning: Can't fix CNI version because the conflist file is not found: $NETWORK_CREATE_OUTPUT"
+    echo "Warning: Please fix the CNI version manually. Anyway the process will continue."
+  fi
+fi
+
 echo
 echo "=========="
-echo "Using compose file:    $COMPOSE_FILE"
+echo "OCI:                   $OCI"
 echo "Using compose command: $COMPOSE"
+echo "Using compose file:    $COMPOSE_FILE"
+echo "Using network name:    $NETWORK_NAME"
 echo "=========="
 echo
 
