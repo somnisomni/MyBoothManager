e9a44a4ce7ea9305ff9fd915de811054075bead3
somni <me@somni.one>
2024-12-21 17:53:26 +0900

    admin: Persist show booth info menu list group in nav

--- projects/Admin/src/pages/AdminLayout.vue
+++ projects/Admin/src/pages/AdminLayout.vue
@@ -12,43 +12,80 @@
     <VNavigationDrawer v-model="navOpen"
                        :permanent="navPersistent"
                        class="navdrawer-flex">
-      <VList v-model:opened="navOpenedMenuGroups" nav class="overflow-y-auto">
-        <VListItem prepend-icon="mdi-cash-register" title="현장 판매 모드 (POS)" value="pos"
-                   :to="{ name: 'admin-pos' }" exact
+      <VList v-model:opened="navOpenedMenuGroups"
+             nav
+             color="secondary"
+             class="overflow-y-auto">
+        <VListItem value="pos"
+                   prepend-icon="mdi-cash-register"
+                   title="현장 판매 모드 (POS)"
+                   :to="{ name: 'admin-pos' }"
+                   exact
                    :disabled="!currentBoothIsOpened"
                    :subtitle="!currentBoothIsOpened ? '부스가 운영 중이어야 합니다.' : undefined" />
 
         <VListSubheader>관리</VListSubheader>
-        <VListItem prepend-icon="mdi-view-dashboard" title="대시보드" value="dashboard"
-                   :to="{ name: 'admin' }" exact />
-        <!-- <VListItem prepend-icon="mdi-store-cog" title="부스 정보 / 인포" value="info"
-                   :to="{ name: 'admin-info' }" exact /> -->
+        <VListItem value="dashboard"
+                   prepend-icon="mdi-view-dashboard"
+                   title="대시보드"
+                   :to="{ name: 'admin' }"
+                   exact />
         <VListGroup value="info-group">
           <template #activator="{ props }">
             <VListItem v-bind="props"
-                        prepend-icon="mdi-store-cog"
-                        title="부스 정보" />
+                       prepend-icon="mdi-store-cog"
+                       append-icon="null"
+                       class="no-interaction"
+                       :active="false"
+                       title="부스 정보" />
           </template>
 
-          <VListItem title="부스 등록 정보" value="info"
-                     :to="{ name: 'admin-info-info' }" exact />
-          <VListItem title="공지 사항" value="notice"
-                     :to="{ name: 'admin-info-notice' }" exact />
-          <VListItem title="멤버" value="member"
-                     :to="{ name: 'admin-info-member' }" exact />
-          <VListItem title="배너 이미지" value="banner-image"
-                     :to="{ name: 'admin-info-banner-image' }" exact />
-          <VListItem title="인포 이미지" value="info-image"
-                     :to="{ name: 'admin-info-info-image' }" exact />
+          <VListItem value="info"
+                     title="부스 등록 정보"
+                     :to="{ name: 'admin-info-info' }"
+                     exact
+                     density="compact" />
+          <VListItem value="notice"
+                     title="공지 사항"
+                     :to="{ name: 'admin-info-notice' }"
+                     exact
+                     density="compact" />
+          <VListItem value="member"
+                     title="멤버"
+                     :to="{ name: 'admin-info-member' }"
+                     exact
+                     density="compact" />
+          <VListItem value="banner-image"
+                     title="배너 이미지"
+                     :to="{ name: 'admin-info-banner-image' }"
+                     exact
+                     density="compact" />
+          <VListItem value="info-image"
+                     title="인포 이미지"
+                     :to="{ name: 'admin-info-info-image' }"
+                     exact
+                     density="compact" />
         </VListGroup>
-        <VListItem prepend-icon="mdi-gift-open" title="굿즈" value="goods"
-                   :to="{ name: 'admin-goods' }" exact />
-        <VListItem prepend-icon="mdi-receipt-text" title="판매 기록" value="orders"
-                   :to="{ name: 'admin-orders' }" exact />
-        <VListItem prepend-icon="mdi-chart-bar" title="통계" value="analytics"
-                   :to="{ name: 'admin-analytics' }" exact />
-        <VListItem prepend-icon="mdi-cash-register" title="정산" value="closing"
-                   :to="{ name: 'admin-closing' }" exact />
+        <VListItem value="goods"
+                   prepend-icon="mdi-gift-open"
+                   title="굿즈"
+                   :to="{ name: 'admin-goods' }"
+                   exact />
+        <VListItem value="orders"
+                   prepend-icon="mdi-receipt-text"
+                   title="판매 기록"
+                   :to="{ name: 'admin-orders' }"
+                   exact />
+        <VListItem value="analytics"
+                   prepend-icon="mdi-chart-bar"
+                   title="통계"
+                   :to="{ name: 'admin-analytics' }"
+                   exact />
+        <VListItem value="closing"
+                   prepend-icon="mdi-cash-register"
+                   title="정산"
+                   :to="{ name: 'admin-closing' }"
+                   exact />
 
         <VDivider />
 
@@ -59,21 +96,26 @@
         <VDivider /> -->
 
         <VListSubheader>지원</VListSubheader>
-        <VListItem prepend-icon="mdi-comment-quote" title="피드백"
+        <VListItem prepend-icon="mdi-comment-quote"
+                   title="피드백"
                    @click="isFeedbackDialogOpen = true" />
-        <VListItem prepend-icon="mdi-help-circle" title="도움말" value="help"
+        <VListItem value="help"
+                   prepend-icon="mdi-help-circle"
+                   title="도움말"
                    :to="{ name: 'admin-support-help' }" />
 
         <VDivider />
 
         <VListSubheader>계정</VListSubheader>
-        <VListItem density="compact" min-height="30px">
+        <VListItem density="compact"
+                   min-height="30px">
           <div class="d-flex flex-column text-subtitle-2 text-disabled">
             <span>현재 로그인 계정: {{ currentAccount?.name }}</span>
             <span style="font-size: 0.8em">로그인 ID: {{ currentAccount?.loginId }}</span>
           </div>
         </VListItem>
-        <VListItem prepend-icon="mdi-logout" title="로그아웃" value="logout"
+        <VListItem prepend-icon="mdi-logout"
+                   title="로그아웃"
                    :href="logoutPageHref" />
       </VList>
 
@@ -83,7 +125,7 @@
         <VListItem density="compact" min-height="30px">
           <div v-if="isDevEnv" class="text-subtitle-2 text-disabled text-center">개발 환경에서 실행 중</div>
           <div v-else-if="isStagingEnv" class="text-subtitle-2 text-disabled text-center">테스트를 위해 배포 중인 사이트입니다. 수시로 기능이 변경되거나 각종 버그 발생, 또는 작동이 멈출 수 있습니다.</div>
-          
+
           <div class="text-subtitle-2 text-disabled text-center">{{ APP_VERSION }} <small>({{ GIT_HASH }})</small></div>
         </VListItem>
         <VListItem prepend-icon="mdi-open-in-new" title="부스 공개 페이지 열기" :href="boothPublicPageHref" target="_blank"
@@ -102,7 +144,7 @@
 
 <script lang="ts">
 import { APP_NAME, BoothStatus, type IAccount, type IBooth } from "@myboothmanager/common";
-import { Vue, Component, Setup } from "vue-facing-decorator";
+import { Vue, Component, Setup, Watch } from "vue-facing-decorator";
 import { useDisplay } from "vuetify";
 import { useAdminStore } from "@/plugins/stores/admin";
 import BoothSelectionArea from "@/components/navbar/BoothSelectionArea.vue";
@@ -135,7 +177,13 @@ export default class AdminLayout extends Vue {
   logoutPageHref = router.resolve({ name: "logout" }).href || "/logout";
 
   isFeedbackDialogOpen = false;
-  navOpenedMenuGroups: string[] = [ "info-group" ];
+  get navOpenedMenuGroups(): string[] {
+    return [ "info-group" ];
+  }
+  set navOpenedMenuGroups(value: string[]) { /* do nothing */ }
+
+  @Watch("navOpenedMenuGroups")
+  test() { console.log(this.navOpenedMenuGroups); }
 
   set navOpen(value: boolean) { this._navOpen = value; }
   get navOpen() {
